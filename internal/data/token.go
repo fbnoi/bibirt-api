49ba44e6595ed32c37e862fbe319b35dd1b7866d
浪 <fbnoi@foxmail.com>
2023-09-07 22:17:13 +0800

    Update

--- internal/data/token.go
+++ internal/data/token.go
@@ -13,8 +13,6 @@ import (
 const (
 	TOKEN_BLACK_LIST_NAMESPACE = "token:black_list:"
 	TOKEN_USER_NAMESPACE       = "token:user"
-
-	TOKEN_BLOCKED_FIELD = "blocked"
 )
 
 type ErrAble interface {
@@ -23,20 +21,18 @@ type ErrAble interface {
 
 type TokenRepo struct {
 	rdb *redis.Client
-	log *log.Helper
 }
 
 func NewTokenRepo(data *Data, logger log.Logger) biz.TokenRepo {
 	return &TokenRepo{
 		rdb: data.rdb,
-		log: log.NewHelper(logger),
 	}
 }
 
 func (tr *TokenRepo) RegisterToken(tok *jwt.Token) {
 	claims := tok.Claims.(jwt.RegisteredClaims)
 	UUID := claims.Audience[0]
-	tr.logErr(
+	tr.must(
 		tr.rdb.HSet(
 			fmt.Sprintf("%s:%s", TOKEN_USER_NAMESPACE, UUID),
 			claims.Subject,
@@ -44,46 +40,50 @@ func (tr *TokenRepo) RegisterToken(tok *jwt.Token) {
 		),
 	)
 	key := fmt.Sprintf("token:%s:%s", claims.Subject, claims.ID)
-	tr.logErr(tr.rdb.Set(key, 0, time.Until(claims.ExpiresAt.Time)))
+	tr.must(tr.rdb.Set(key, 0, time.Until(claims.ExpiresAt.Time)))
 }
 
 func (tr *TokenRepo) BlockToken(tok *jwt.Token) {
 	claims := tok.Claims.(jwt.RegisteredClaims)
 	key := fmt.Sprintf("token:%s:%s", claims.Subject, claims.ID)
-	tr.logErr(tr.rdb.Set(key, 1, 0))
+	tr.must(tr.rdb.Set(key, 1, 0))
 }
 
 func (tr *TokenRepo) BlockTokenByUUIDAndSubject(uuid, subject string) {
-	if id, err := tr.getUserTokenID(uuid, subject); err == nil {
-		key := fmt.Sprintf("token:%s:%s", subject, id)
-		tr.logErr(tr.rdb.Set(key, 1, 0))
-	} else {
-		tr.log.Error(err)
-	}
+	id := tr.getUserTokenID(uuid, subject)
+	key := fmt.Sprintf("token:%s:%s", subject, id)
+	tr.must(tr.rdb.Set(key, 1, 0))
 }
 
-func (tr *TokenRepo) getUserTokenID(UUID, field string) (string, error) {
+func (tr *TokenRepo) getUserTokenID(UUID, field string) string {
 	key := fmt.Sprintf("%s:%s", TOKEN_USER_NAMESPACE, UUID)
-	return tr.rdb.HGet(key, field).Result()
+	id, err := tr.rdb.HGet(key, field).Result()
+	if err != nil {
+		panic(err)
+	}
+
+	return id
 }
 
 func (tr *TokenRepo) IsTokenBlocked(tok *jwt.Token) bool {
 	claims := tok.Claims.(jwt.RegisteredClaims)
 	key := fmt.Sprintf("token:%s:%s", claims.Subject, claims.ID)
-	if res, err := tr.rdb.Exists(key).Result(); err == nil && res > 0 {
-		if i, err := tr.rdb.Get(key).Int(); err == nil {
-			return i == 1
-		} else {
-			tr.log.Error(err)
-		}
-	} else {
-		tr.log.Error(err)
+	res, err := tr.rdb.Exists(key).Result()
+	if err != nil {
+		panic(err)
 	}
-	return true
+	if res <= 0 {
+		return false
+	}
+	i, err := tr.rdb.Get(key).Int()
+	if err != nil {
+		panic(err)
+	}
+	return i == 1
 }
 
-func (tr *TokenRepo) logErr(errAble ErrAble) {
+func (tr *TokenRepo) must(errAble ErrAble) {
 	if err := errAble.Err(); err != nil {
-		tr.log.Error(err)
+		panic(err)
 	}
 }
